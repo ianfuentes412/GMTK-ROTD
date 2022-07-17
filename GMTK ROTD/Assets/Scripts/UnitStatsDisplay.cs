c8d2094cfa0c5ba4880df0c73301e64bf5301854
Ian Fuentes <66396674+ianfuentes412@users.noreply.github.com>
2022-07-17 06:01:56 -0700

    ScriptObject

--- GMTK ROTD/Assets/Scripts/UnitStatsDisplay.cs
+++ GMTK ROTD/Assets/Scripts/UnitStatsDisplay.cs
@@ -2,14 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UnitStatsDisplay : MonoBehaviour
 {
     public UnitScrptObjct unit;
-    public Text nameText;
-    public Text healthText;
-    public Text attackText;
-    public Text speedText;
+    public TMP_Text nameText;
+    public TMP_Text healthText;
+    public TMP_Text attackText;
+    public TMP_Text speedText;
     // Start is called before the first frame update
     void Start()
     {
